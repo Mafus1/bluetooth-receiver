fdbd3e7e60e2a08141ba867f669689875e2367bb
Fabian <maurefab@students.zhaw.ch>
2016-12-15 12:58:44 +0100

    Added byte countdown to baud tick generator.

--- source/baud_tick.vhd
+++ source/baud_tick.vhd
@@ -4,30 +4,31 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
 -- Entity Declaration 
-ENTITY boud_tick IS
+ENTITY baud_tick IS
   PORT( 
 		clk			:	IN		std_logic;
 		baud_tick	:	OUT	std_logic;
 		reset_n		:	IN		std_logic;
 		start_bit   :	in		std_logic
 	);
-END boud_tick;
+END baud_tick;
 
 
 -- Architecture Declaration 
-ARCHITECTURE rtl OF boud_tick IS
+ARCHITECTURE rtl OF baud_tick IS
 	
-	CONSTANT	period 				:	unsigned(9 downto 0) := to_unsigned(434,9); 
-	CONSTANT	init_period		 	:	unsigned(9 downto 0) := to_unsigned(651,9); 
-	SIGNAL 	count, next_count	:	unsigned(9 downto 0);
+	CONSTANT	period 						:	unsigned(9 downto 0) := to_unsigned(434, 9); 
+	CONSTANT	init_period		 			:	unsigned(9 downto 0) := to_unsigned(651, 9); 
+	CONSTANT init_cntdown				:  unsigned(3 downto 0) := to_unsigned(7, 4);
+	SIGNAL 	count, next_count			:	unsigned(9 downto 0);
+	SIGNAL   cntdown, next_cntdown	:  unsigned(3 downto 0); 
 	
 -- Begin Architecture
 BEGIN 
 
-	--------------------------------------------------
-	-- PROCESS FOR COMBINATORIAL LOGIC
-	--------------------------------------------------
-	comb_logic: PROCESS(count, start_bit)
+
+	-- input logic for baud tick
+	input_logic: PROCESS(count, start_bit)
 	BEGIN	
 		IF (start_bit = '1') THEN
 			next_count <= init_period;
@@ -41,31 +42,49 @@ BEGIN
 			next_count <= period;
 		END IF;
 
-	END PROCESS comb_logic;   
+	END PROCESS input_logic;   
 
-	 -------------------------------------------
-    -- Process for registers
-    -------------------------------------------
-	flip_flops : PROCESS(clk, reset_n)
+	-- baud tick period countdown
+	baud_flip_flops : PROCESS(clk, reset_n)
 	BEGIN	
 		IF reset_n = '0' THEN
 			count <= init_period;
 		ELSIF rising_edge(clk) THEN
 			count <= next_count;
 		END IF;
-	END PROCESS flip_flops;	
+	END PROCESS baud_flip_flops;	
 	
-	--------------------------------------------------
-	-- PROCESS FOR COMBINATORIAL LOGIC
-	--------------------------------------------------
-	comb_logic_out: PROCESS(count)
+	-- baud tick output logic
+	output_logic: PROCESS(count)
 	BEGIN
-		if(count = 0) THEN
+		IF (count = 0 AND cntdown > 0) THEN
 			baud_tick <= '1';
 		ELSE
 			baud_tick <= '0';
 		END IF;
 		
-	END PROCESS comb_logic_out;
+	END PROCESS output_logic;
+	
+	-- byte countdown logic
+	countdown_logic: PROCESS(start_bit, count, cntdown)
+	BEGIN
+		IF (start_bit = '1') THEN
+			next_cntdown <= init_cntdown;
+		ELSIF (count = 0 AND cntdown > 0) THEN
+			next_cntdown <= cntdown - 1;
+		ELSIF (count = 0 AND cntdown = 0) THEN
+			next_cntdown <= to_unsigned(0, 4);
+		END IF;
+	END PROCESS countdown_logic;
+	
+	-- byte countdown flip flops
+	countdown_flop_flops: PROCESS(clk, count, reset_n)
+	BEGIN
+		IF reset_n = '0' THEN
+			cntdown <= to_unsigned(0, 4);
+		ELSIF rising_edge(clk) THEN
+			cntdown <= next_cntdown;
+		END IF;
+	END PROCESS countdown_flop_flops;
 	
 END rtl;	
